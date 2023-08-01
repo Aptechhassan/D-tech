27fff3bbfd03ffdb0ca5d46db9b7d69238cb7999
Hassan <hassan03152530851@gmail.com>
2023-08-01 13:59:43 +0500

    forth

--- javascript.js
+++ javascript.js
@@ -1,26 +1,33 @@
 // j Query code 
 
-  $(document).ready(function(){
-    $(window).scroll(function(){
-      var scroll=(window).scrollTop();
-      if(scroll > 300){
-        $(".red").css("background" ,"black");
-        
-      }
-      else{
-        $(".red").css("background" ,"white");
-          $ (".red").css("color" ,"black");
-      }
-    })
-  })
 
 
-   var count =setInterval(updated);
-   var upto = 0;
-   function updated(){
-    var counter = document.getElementById("#counter-up1");
-      counter.innerHTML=++upto;
-  if(upto === 10){
-    clearInterval(count);
-  } 
-   }
+
+ //  var count =setInterval(updated);
+ //  var upto = 0;
+ //  function updated(){
+ //   var counter = document.getElementById("counter1");
+ //     counter.innerHTML=++upto;
+ // if(upto === 250){
+ //   clearInterval(count);
+ // } 
+ //  }
+ let counts = setInterval(updated);
+        let upto = 0;
+        function updated() {
+            let count = document.getElementById("counter");
+            count.innerHTML = ++upto;
+            if (upto === 10) {
+                clearInterval(counts);
+            }
+        }
+
+        let count = setInterval(updated);
+        let aupto = 0;
+        function updated() {
+            let count = document.getElementById("counter3");
+            count.innerHTML = ++aupto;
+            if (aupto === 950) {
+                clearInterval(counts);
+            }
+        }
